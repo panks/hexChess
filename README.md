e5ecbdba5a70e14ea3c10a48beb39d6cc5887a76
Pankaj Kumar <me@panks.me>
2012-12-01 19:20:37 +0530

    Include Comment and Update README

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ hexChess
 ========
 
 Hexagonal Chess in Prolog
+
+Usages:
+To start the game, input these two commands in prolog terminal:
+
+consult(board).
+homePlay(2).
+
+
+
+Authors:
+Pankaj Kumar (me@panks.me)
+Vageesh DC (vageesh.dc@gmail.com)
--- board.pl
+++ board.pl
@@ -1,17 +1,18 @@
-
-include('moves.pl').
 /*
-a([wr,wn,wb,wq,wk,wb,wn,wb,wr]).
-b([wp,wp,wp,wp,wp,wp,wp,wp,wp]).
-c([.,.,.,.,.,.,.,.,.]).
-d([.,.,.,.,.,.,.,.,.]).
-e([.,.,.,.,.,.,.,.,.]).
-f([br,bp,.,.,.,.,.,bp,br]).
-g([x,bb,bp,.,.,.,bp,bn,x]).
-h([x,x,bn,bp,.,bp,bb,x,x]).
-i([x,x,x,bb,bp,bq,x,x,x]).
-j([x,x,x,x,bk,x,x,x,x]).
+Team Members:
+Vageesh DC (CS10B027)
+Pankaj Kumar (CS10b042)
 
+Usages:
+To start the game, enter these two commands in prolog terminal---
+
+Consult(board).
+homePlay(2).
+
+*/
+
+
+/*
 Starting Board
 big(
 [
@@ -31,6 +32,7 @@ big(
 
 */
 
+% zeros denote places not present in the board, they are just to make it easier to code, 1 denote the empty places.
 
 big(
 [
@@ -73,21 +75,9 @@ big3(
   [0,0,0,0,20,0,0,0,0]
 ]).
 
-% A([a,v,c]).
-/*
-%%% get position here in X<Y matrix notation!!
-pos(X,Y):-X == 1,a(Z),posY(Y,Z).
-pos(X,Y):-X == 2,b(Z),posY(Y,Z).
-pos(X,Y):-X == 3,c(Z),posY(Y,Z).
-pos(X,Y):-X == 4,d(Z),posY(Y,Z).
-pos(X,Y):-X == 5,e(Z),posY(Y,Z).
-pos(X,Y):-X == 6,f(Z),posY(Y,Z).
-pos(X,Y):-X == 7,g(Z),posY(Y,Z).
-pos(X,Y):-X == 8,h(Z),posY(Y,Z).
-pos(X,Y):-X == 9,i(Z),posY(Y,Z).
-pos(X,Y):-X == 10,j(Z),posY(Y,Z).
-%pos(X,Y):- print('ERR').
-*/
+
+
+
 pos(X,Y):-big(D),posX(X,Y,D).
 
 posX(1,Y,[H|_]):-posY(Y,H).
@@ -96,14 +86,12 @@ posX(X,Y,[H|J]):-Y1 is X-1,posX(Y1,Y,J).
 posY(1,[H|_]):-print(H).
 posY(Y,[H|K]):-Y1 is Y-1, posY(Y1,K).
 
-%%% DONW WITH POSITIONS
-%%% rules for white pawns
-
+% Move a piece from X1,X2 to Y1,Y2
 move(X1,Y1,X2,Y2,D,Dnew):-get(X1,Y1,P,D),set(X1,Y1,1,D,D1),set(X2,Y2,P,D1,Dnew).
 
 testGet(X,Y,Z):-big(D),get(X,Y,Z,D).
 
-%get pos from board
+%get piece from the board given positin and board
 get(X,Y,P,D):-getX(X,Y,P,D).
 getX(1,Y,P,[H|_]):-getY(Y,P1,H),P is P1.
 getX(X,Y,P,[H|J]):-X1 is X-1,getX(X1,Y,P1,J),P is P1.
@@ -113,18 +101,16 @@ getY(Y,P,[H|K]):-Y1 is Y-1,getY(Y1,P1,K),P is P1.
 
 testSet(X,Y,P,Dn):-big(D),set(X,Y,P,D,Dn).
 
-%set pos in board
+%set piece at a particular position in board
 set(X,Y,P,D,Dn):-setX(X,Y,P,D,[],Dn).%last 2 are buffers, next to D is the result!!
 setX(1,Y,P,[H|J],T,Re):-set_Y(Y,P,H,Z),append(T,[Z],Re1),append(Re1,J,Re).
 setX(X,Y,P,[H|J],T,Re):-X1 is X-1,append(T,[H],Tnew),setX(X1,Y,P,J,Tnew,Re).
-
 set_Y(Y,P,H,Z):-setY(Y,P,H,[],Z).
-%setY(1,P,[H|B],[],Re):-append([],[P],Re1),append(Re1,B,Re).%Re1 is [T|P],Re is [Re1|B].
 setY(1,P,[H|B],T,Re):-append(T,[P],Re1),append(Re1,B,Re).%Re1 is [T|P],Re is [Re1|B].
 setY(Y,P,[H|B],T,Re):-Y1 is Y-1,append(T,[H],Tnew),setY(Y1,P,B,Tnew,Re).
 
 
-%%%
+%%% Moves----
 %knight
 knight(X1,Y1,D,X2,Y2,L):-X2 is X1+2,Y2 is Y1-1,get(X2,Y2,P,D),(P =1;L is P//10),(X2 \== X1;Y2 \== Y1).
 knight(X1,Y1,D,X2,Y2,L):-X2 is X1+2,Y2 is Y1+1,get(X2,Y2,P,D),(P =1;L is P//10),(X2 \== X1;Y2 \== Y1).
@@ -138,7 +124,6 @@ knight(X1,Y1,D,X2,Y2,L):-X2 is X1-3,Y2 is Y1-2,get(X2,Y2,P,D),(P =1;L is P//10),
 %knight(X1,Y1,D,X2,Y2,L):-MT = MM. MT = [[X2,Y2]|MM]
 
 %queen
-
 queen(X1,Y1,D,X2,Y2,L):-rook(X1,Y1,D,X2,Y21,L),(X2 \== X1;Y2 \== Y1).
 queen(X1,Y1,D,X2,Y2,L):-bishop(X1,Y1,D,MT1,MT,L),(X2 \== X1;Y2 \== Y1).
 %queen(X1,Y1,D,X2,Y2,L):-MT = MM.
@@ -241,8 +226,6 @@ rook(X1,Y1,D,X2,Y2,L):-X2 is X1-9,Y2 is Y1+9,get(X2,Y2,P,D),(P =1;L is P//10),(X
 rook(X1,Y1,D,X2,Y2,L):-X2 is X1+9,Y2 is Y1-9,get(X2,Y2,P,D),(P =1;L is P//10),(X2 \== X1;Y2 \== Y1).
 rook(X1,Y1,D,X2,Y2,L):-X2 is X1-9,Y2 is Y1-9,get(X2,Y2,P,D),(P =1;L is P//10),(X2 \== X1;Y2 \== Y1).
 
-%%%% ---
-
 %bishop
 bishop(X1,Y1,D,X2,Y2,L):-X2 is X1,Y2 is Y1+2,get(X2,Y2,P,D),(P =1;L is P//10),(X2 \== X1;Y2 \== Y1).
 bishop(X1,Y1,D,X2,Y2,L):-X2 is X1,Y2 is Y1-2,get(X2,Y2,P,D),(P =1;L is P//10),(X2 \== X1;Y2 \== Y1).
@@ -291,26 +274,28 @@ myTurn(Turn,Board):-read(X),getIpParts(X,X1,Y1,X2,Y2),move(X1,Y1,X2,Y2,Board,New
 
 getIpParts([X,Y,X1,Y1],A,B,C,D):-A is X,B is Y,C is X1,D is Y1.
 
-%play(Turn,Board):-read(X), splitInput(X,X1,Y1,X2,Y2,Turn,Board), move(X1,Y1,X2,Y2,Board,NewBoard), chooseMove(NewBoard, Turn, NewBoard2), Turn is Turn + 2, play(Turn, NewBoard2). 
 
 %TODO Write endgame function
-%input usage for moving king from d3 to c5 - [k,d,3,c,5].
-%homePlay(T):-big(B),play(1,B),T is 1.
-homePlay(T):-big(B),playDual(1,B),T is 2.
+
+% Call this to start the game.
+homePlay(T):-write('Usage: to move a pawn from a,2 a,3 input [p,a,2,a,3] for king input [k,a,2,a,3]. Positions on board with 1 are empty positions and with 0 are non-existent'),nl,big(B),playDual(1,B),T is 2.
 
 play(Turn,Board):- write('Play your move..'),nl, read(X), splitInput2(X,X1,Y1,X2,Y2,Turn,Board), move(X1,Y1,X2,Y2,Board,NewBoard), 
                    Turn2 is Turn + 1, write('Wait for Computer to Play..'), chooseMove(NewBoard, Turn2, NewBoard2), write('Done..'), nl,Turn3 is Turn2 + 1, play(Turn3, NewBoard2). 
 
-%two person!!
 playDual(Turn,Board):-player1(Turn,Board,Boardout,Turnout),printb(Boardout),nl,write('Wait for opponent to Play..'),player2(Turnout,Boardout,Boardout1,Turnout1),printb(Boardout1),nl,write('done'),nl,playDual(1,Boardout1).
 
 /*
 playDual(Turn,Board):- write('Play your move..'),nl, read(X), splitInput2(X,X1,Y1,X2,Y2,Turn,Board), move(Y1,X1,Y2,X2,Board,NewBoard), Turn2 is Turn + 1, 
                        write(NewBoard),write('Wait for opponent to Play..'),nl, read(AX), splitInput2(AX,AX1,AY1,AX2,AY2,Turn2,NewBoard),
                        move(AY1,AX1,AY2,AX2,NewBoard,NewBoard2),write('Done..'), nl,Turn3 is Turn2 + 1, write(NewBoard2),playDual(Turn3, NewBoard2). 
-*/                       
+*/
+
+%Player with input from keyboard
 player1(Turn,Board,NewBoard,Turn2):-write('Play your move..'),nl, read(X), splitInput2(X,X1,Y1,X2,Y2,Turn,Board),isValid(Y1,X1,Y2,X2,Board,Turn), move(Y1,X1,Y2,X2,Board,NewBoard), Turn2 is Turn + 1.
 
+
+% Cheching if a move is valid, given Move, Board and Turn (which player made the move).
 isValid(Y1,X1,Y2,X2,Board,Turn):-pawn(Y1,X1,Board,Y2,X2,Turn).
 isValid(Y1,X1,Y2,X2,Board,Turn):-rook(Y1,X1,Board,Y2,X2,Turn).
 isValid(Y1,X1,Y2,X2,Board,Turn):-queen(Y1,X1,Board,Y2,X2,Turn).
@@ -346,58 +331,43 @@ flatten2([L|Ls], FlatL) :-
     
 flatten2(L, [L]).
 
-/* DANGER 
-  implement value Function!!!*/
-
 finAI([[I,J,X,Y]|Moves],Board,Best,Topscore,Turn):-move(I,J,X,Y,Board,Board1),eval(Score,Turn,Board1),Score >= Topscore,write('asdf '),write(Score),nl,
                                               Topscore1 is Score,Best = [I,J,X,Y],finAI(Moves,Board,Best,Topscore1,Turn).
                                               
 finAI([],Board,Best,T,Turn).
   
 evaluate_and_choose([[I,J,X,Y]|Moves],Position,D,MaxMin,Record,BestMove,Turn) :-
-%nl,write(Moves),nl,write(I),write(J),nl,write(Position),nl,
-write(' in eval_and_choose'),
 	move(I,J,X,Y,Position,Position1),
     minimax1(D,Position1,MaxMin,_,Value,Turn), 
 	update([I,J,X,Y],Value,Record,Record1),
-    write(' eval innercall'),
  	evaluate_and_choose(Moves,Position,D,MaxMin,Record1,BestMove,Turn).
     
-%evaluate_and_choose([[A,B,[],D]|Moves],Position,D,MaxMin,Record,BestMove,T):-evaluate_and_choose(Moves,Position,D,MaxMin,Record,BestMove,Turn).
-%evaluate_and_choose([[A,B,C,[]]|Moves],Position,D,MaxMin,Record,BestMove,T):-evaluate_and_choose(Moves,Position,D,MaxMin,Record,BestMove,Turn).
-%evaluate_and_choose([[A,B,[],[]]|Moves],Position,D,MaxMin,Record,BestMove,T):-evaluate_and_choose(Moves,Position,D,MaxMin,Record,BestMove,Turn).
 evaluate_and_choose([],Position,D,MaxMin,Record,Record,T).	
 
- %%%%%%%%%%%%%%%%
-minimax1(0,Position,MaxMin,Move,Value,Turn) :-write(' in Maxmin first'),eval(Score,Turn,Position),Value is Score * MaxMin.
+
+% MinMax implementation to check for best move for computer.
+
+minimax1(0,Position,MaxMin,Move,Value,Turn) :-eval(Score,Turn,Position),Value is Score * MaxMin.
      
 minimax1(D,Position,MaxMin,Move,Value,Turn) :-
-        write(' in Maxmin 2'),
         flip(Turn,Turn1),
         D > 0,
-        %findall(M,move(Position,M),Moves),%%HOW TO DO THIS
         lopX(1,1,10,9,Position,Turn1,Moves),%,flatten2(Lis,Moves), %change here for testing!!
         D1 is D - 1,
-        MinMax is -MaxMin,write(D),
+        MinMax is -MaxMin,
         evaluate_and_choose(Moves,Position,D1,MinMax,(nil,-999999),(Move,Value),Turn1).
 
         %%%%%%%%%%%
 update(Move,Value,(Move1,Value1),(Move1,Value1)) :- Value =< Value1.
-%update(Move,Value,(Move1,Value1),(Move1,Value1)) :- Value == Value1.
 update(Move,Value,(Move1,Value1),(Move,Value)) :- Value > Value1.
 	  
-	  %%%%%%%%%%%%%%%%%%%%%%%%%
-	  /*
-	   big(B),lopX(1,1,10,9,B,2,M),evaluate_and_choose(M,B,4,-1,(nil,-9999999),(X,Y),2).
-	  
-	  evaluate_and_choose([[1,2,3,3],[1,3,1,1],[1,3,3,7],[1,3,4,9],[1,4,_288610,_288612],[1,4,_288619,_288621],[1,4,_288628,_288630],[1,4,_288637,_288639],[1,4,_288646,_288648],[1,4,_288655,_288657],[1,4,1,_288810],[1,4,1,_288891],[1,4,3,_288819],[1,4,3,_288828],[1,4,3,_288837],[1,4,3,_288846],[1,4,3,_288855],[1,4,3,_288864],[1,4,3,_288873],[1,4,3,_288882],[1,4,4,_288738],[1,4,4,_288747],[1,4,4,_288756],[1,4,4,_288765],[1,4,4,_288774],[1,4,4,_288783],[1,4,4,_288792],[1,4,4,_288801],[1,4,5,_288702],[1,4,5,_288711],[1,4,5,_288720],[1,4,5,_288729],[1,4,6,_288666],[1,4,6,_288675],[1,4,6,_288684],[1,4,6,_288693],[1,5,1,6],[1,7,3,6],[1,7,3,8],[1,8,1,6],[1,8,3,4],[1,8,4,2],[1,9,1,1],[1,9,1,6],[1,9,3,7],[1,9,4,6],[1,9,5,5],[1,9,6,4],[1,9,7,3],[2,1,3,1],[2,2,3,2],[2,2,4,2],[2,3,3,3],[2,3,4,3],[2,4,3,4],[2,4,4,4],[2,4,5,4],[2,5,3,5],[2,5,4,5],[2,5,5,5],[2,6,1,6],[2,6,3,6],[2,6,4,6],[2,6,5,6],[2,7,3,7],[2,7,4,7],[2,8,3,8],[2,8,4,8],[2,9,3,9]],[[27,13,15,19,10,1,13,15,17],[11,11,11,11,11,11,11,11,11],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[21,1,1,1,1,1,1,1,21],[1,21,1,1,1,1,1,21,27],[0,25,21,1,1,1,21,23,0],[0,0,23,21,1,21,25,0,0],[0,0,0,25,25,29,0,0,0],[0,0,0,0,20,0,0,0,0]],1,1,([1,2,3,1],-15),(_286134,_380419),1)
-	  */
-player2(Turn,Board,NewBoard,Turn2):-lopX(1,1,10,9,Board,Turn,Moves),nl,write('in play2'),%flatten2(Lis,Moves),
+% AI Player
+player2(Turn,Board,NewBoard,Turn2):-lopX(1,1,10,9,Board,Turn,Moves),%flatten2(Lis,Moves),
                                     %evaluate_and_choose(Moves,Board,1,-1,(nil,-999999),([I,J,X,Y],_),Turn),
                                     % finAI(Moves,Board,[I,J,X,Y],T,Turn),
                                     % move(I,J,X,Y,Board,NewBoard),Turn2 is Turn +1.
                                     chooseR(Turn,Board,Moves, _,[I,J,X,Y]), 
-                                    move(I,J,X,Y,Board,NewBoard),Turn2 is Turn +1, nl, write('Move is: '),write(J),write(I),write(Y), write(X), nl.
+                                    move(I,J,X,Y,Board,NewBoard),Turn2 is Turn +1, nl, write('Move is: '),write(J),write(', '),write(I),write(' to '),write(Y),write(', '), write(X), nl.
 
 chooseR(_,_,[], [], []).
 chooseR(Turn,Board,List, Elt, RElt) :-
@@ -408,9 +378,6 @@ chooseR(Turn,Board,List, Elt, RElt) :-
 
                                    
 %TODO Write endgame function
-%input usage for moving king from d3 to c5 - [k,d,3,c,5].	
-
-%play(Turn,Board):- write('Play your move..'), big3(Board), read(X), splitInput2(X,X1,Y1,X2,Y2,Turn,Board), move(X1,Y1,X2,Y2,Board,NewBoard), Turn2 is Turn + 1, write('Wait for Computer to Play..'), chooseMove(NewBoard, Turn2, NewBoard2), write('Done..'), Turn3 is Turn2 + 1, play(Turn3, NewBoard2). 
 
 valcomp(Turn,Board,A):- catch(valcomp2(Turn,Board,A), E, false).
 valcomp2(Turn,Board,[A|[B|[C|[D|E]]]]):- C < 10, D < 9, get(A,B,P,Board), M is P//10, M == Turn, nl, write('M and Turn are: '), write(M), write(Turn),write(' get it:'),write(P),nl.
@@ -434,6 +401,7 @@ nine(i).
 
 isBlack(0).
 
+% Parse input given by user and convert to row/column number.
 splitInput([X|[B|[R|D]]], X1, Y1, X2, Y2, Turn, Board):-   (
                 ki(X) -> P = 'king', Val = 10;    (
                                             qu(X) -> P='queen', Val = 19;     (
@@ -524,11 +492,8 @@ endGame(Board,Turn).
 
 testChooseMove(A):-big1(B),iterateOverBoard(A,2,X1,Y1,X2,Y2,B,1,1,1,B),print(X1),nl,print(Y1),nl,print(X2),nl,print(Y2),nl.
 
-%%choosing!!
-%chooseMove(Board,Turn,BoardUp):-Ta is Turn mod 2,T is Ta+1, iterateOverBoard(_,1,X1,Y1,X2,Y2,Board,T,1,1,Board),move(X1,Y1,X2,Y2,Board,BoardUp).
-/*
-XXX TODO : oneMove(Board,T,D,M,3,2) -> to oneMove(Board,T,D,M,X,Y):: the correct X Y dimension!!!
-*/
+
+%Choose a move at random and make that and return the resulting board. (Randon AI)
 chooseMove(Board,Turn,BoardUp):-flip(T,Turn),oneMove(Board,T,1,M,3,2),maximumVal(X1,Y1,X2,Y2,-1,M),move(X1,Y1,X2,Y2,Board,BoardUp).
 
 testOneMove(V,D,Im,Jm,M):-big1(B),oneMove(B,V,D,M,Im,Jm).
@@ -541,8 +506,6 @@ exMax(X1a,Y1a,X2a,Y2a,Sa,[]):-X1a is -1,X2a is -1,Y1a is -1,Y2a is -1,Sa is -1.
 exMax(X1a,Y1a,X2a,Y2a,Sa,[X1,Y1,X2,Y2,S]):-X1a is X1,Y1a is Y1,X2a is X2 , Y2a is Y2,Sa is S.
 
 oneMove(Board,Var,D,M,Imax,Jmax):-setof(Mov,loopX(1,1,Imax,Jmax,Board,Var,D,Mov),M).
-%twoMove(Board,Var,D,M,Imax,Jmax):-setof([X,Y,Xd,Yd,S],findMove(X,Y,Board,Var,D,Xd,Yd,S),M).
-%oneMove1(Board,Var,D,Ar):-setof([X1,Y1,X2,Y2,Sout],findMove(X1,X2,Board,Var,D,X2,Y2,Sout),Ar).
 
 loopY(I,J,Imax,Jmax,Board,V,D,Ar):-J<Jmax,findMove(I,J,Board,V,D,Xd,Yd,Sout),J1 is J+1,loopY(I,J1,Imax,Jmax,Board,V,D,Ar1),Sout == -1,Ar = Ar1.%Ar = [[I,J,Xd,Yd,Sout]|Ar1].
 loopY(I,J,Imax,Jmax,Board,V,D,Ar):-J==Jmax,findMove(I,J,Board,V,D,Xd,Yd,Sout),Sout == -1,Ar = [].
@@ -575,6 +538,9 @@ lisSize([],Cou):-Cou is 0.
 flip(V1,V):-1 is V1 mod 2,V is 2.
 flip(V1,V):-0 is V1 mod 2,V is 1.
 
+
+
+% Evaluating the possible moves and getting the best out of those.
 getBest(I,J,[H|T],Board,Xb,Yb,V,D,Score):-[X1,Y1] = H,move(I,J,X1,Y1,Board,Bnew),flip(V3,V),D1 is D-1,eval(Sc,V,Bnew),
                                           %iterateOverBoard(Sn,D1,_,_,_,_,Bnew,V3,1,1,Bnew),Sc1 is Sn+Sc,
                                           Sc1 is Sc,
@@ -591,10 +557,7 @@ getBest(I,J,[H|T],Board,Xb,Yb,V,D,Score):-%getBest(I,J,T,Board,Xb1,Yb1,V,D,Score
                                           getBest(I,J,T,Board,Xb1,Yb1,V,D,Score1),Xb is Xb1,Yb is Yb1,Score is Score1,
                                           Sc1 is Sc,getBest(I,J,T,Board,_,_,V,D,Snext),Sc1 < Snext. 
 
-%testEval(Score,T):-big1(B),eval(Score,T,B).
-%eval(Score,_,_):-random(Val),Val2 is Val *10,Val1 is abs(Val2),Score is round(Val1).
 
-%%write for eval
 testEval(Score,T):-big3(B),eval(Score,T,B).
 
 eval(Score,T1,B):-flip(T1,T2),evalX(Score,T2,B,1).
@@ -605,12 +568,7 @@ evalX(Score,T,[],X):-S is 0,Score is S.
 evalY(S,T1,[H|T],W,V):-T1 is H//10,evalY(S1,T1,T,W,V),S is S1+1.
 evalY(S,T1,[H|T],W,V):-evalY(S1,T1,T,W,V),S is S1.
 evalY(S,T1,[],W,V):-S1 is 0,S = S1.
-/*
-evalY(S,T1,[H|T],W,V):-H >=  T1*10,H < 10+ T1*10,Snew1 is H mod 10,Snew is Snew1*V*W,V1 is V+1,evalY(Snext,T1,T,W,V1),Stmp is Snew + Snext,S is Stmp.
-evalY(S,T1,[H|T],W,V):-H < 10,Snew is H*V*W,V1 is V+1,evalY(Snext,T1,T,W,V1),Stmp is Snew + Snext,S is Stmp.
-evalY(S,T1,[H|T],W,V):-V1 is V+1,evalY(Snext,T1,T,W,V1),Stmp is Snext + 0,S is Stmp.
-evalY(S,T1,[],W,V):-S1 is 0,S = S1.
-*/
+
+% Printing the board.
 printb([]).
 printb([H|T]):- nl, write(H),printb(T).
-/* EXPAND THIS TODO */
